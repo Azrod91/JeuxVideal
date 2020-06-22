266b3a50d7a20d85aba6116a074f456b2a851b47
Anatole Lellouch <anatole123.lellouch@gmail.com>
2020-06-22 17:14:25 +0200

    ajoute header et footer

--- index.php
+++ index.php
@@ -5,7 +5,14 @@
 	<meta charset="utf-8">
 </head>
 <body>
-	<h1>Hello</h1>
+	<header>
+		<h1>Hello</h1>
+	</header>
+	
 	<p>c est un test pour la mise en vente de jeux vidéo et bien plus</p>
+
+	<footer>
+		Contact
+	</footer>
 </body>
 </html>
